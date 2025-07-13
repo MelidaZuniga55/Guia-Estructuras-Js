69ee89f86f353d1c1c9d5b333b9e09c06761626b
MelidaZuniga55 <melidazuniga55@outlook.com>
2025-07-12 19:17:52 -0600

    Solucion de ejercicio 8:tabla de multiplicar

---
+++ ejercicio8.js
@@ -0,0 +1,7 @@
+function tablaMultiplicar(numero) {
+  console.log(`Ejercicio 8: Tabla del ${numero}`);
+  for (let i = 1; i <= 10; i++) {
+    console.log(`${numero} x ${i} = ${numero * i}`);
+  }
+}
+tablaMultiplicar(9);
