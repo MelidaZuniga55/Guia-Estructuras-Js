ac34189ad3b544cb0582e0e091732b2f3cde3ad3
MelidaZuniga55 <melidazuniga55@outlook.com>
2025-07-15 10:38:11 -0600

    Pruebas ejercicio 1: funcion para verificar si es mayor de edad

--- ejercicio1.js
+++ ejercicio1.js
@@ -2,4 +2,4 @@ function verificarEdad(edad) {
   const mensaje = edad >= 18 ? "El usuario es mayor de edad" : "El usuario es menor de edad";
   console.log("Ejercicio 1:", mensaje);
 }
-verificarEdad(40);
+verificarEdad(20);
